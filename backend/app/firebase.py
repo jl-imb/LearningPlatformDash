c4a3621557230f1dc50ec06c3d2ce23087ce8a22
jl-imb <pace-jlimb7@gatech.edu>
2025-07-29 18:35:44 -0700

    hi

--- backend/app/firebase.py
+++ backend/app/firebase.py
@@ -1,24 +1,22 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import os
-import json
 
 if not firebase_admin._apps:
-    if os.getenv("FIREBASE_PRIVATE_KEY"):
-        cred_dict = {
-            "type": "service_account",
-            "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-            "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-            "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
-            "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-            "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-            "token_uri": "https://oauth2.googleapis.com/token"
-        }
-        cred = credentials.Certificate(cred_dict)
-    else:
-        cred = credentials.Certificate("learningplatform-6bf88-firebase-adminsdk-fbsvc-8fe7e3aa65.json")
-    
+    cred_dict = {
+        "type": os.getenv("FIREBASE_TYPE", "service_account"),
+        "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+        "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+        "private_key": os.getenv("FIREBASE_PRIVATE_KEY", "").replace('\\n', '\n'),
+        "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+        "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+        "auth_uri": os.getenv("FIREBASE_AUTH_URI", "https://accounts.google.com/o/oauth2/auth"),
+        "token_uri": os.getenv("FIREBASE_TOKEN_URI", "https://oauth2.googleapis.com/token"),
+        "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL", "https://www.googleapis.com/oauth2/v1/certs"),
+        "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL"),
+    }
+
+    cred = credentials.Certificate(cred_dict)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
